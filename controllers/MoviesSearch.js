a4fb3dabb4e29616446a6cd1237b08a69454118a
tahier-hussain <tahierhussain55@gmail.com>
2020-09-12 03:20:19 +0530

    Completed the entire server end functionalities

--- controllers/MoviesSearch.js
+++ controllers/MoviesSearch.js
@@ -4,11 +4,13 @@ const esClient = new elasticsearch.Client({
   host: "127.0.0.1:9200",
   log: "error"
 });
+const paginate = require("jw-paginate");
 
 //API for Movies Search
 exports.search = (req, res) => {
-  const { title, date_published, duration, director, genre, country, language, sort_duration, sort_duration_asc, sort_date, sort_date_asc } = req.query;
+  const { page, title, date_published, duration, director, genre, country, language, sort_duration, sort_duration_asc, sort_date, sort_date_asc } = req.query;
 
+  console.log(req.query);
   let query = {};
   let sort = [];
   let must_index = 0;
@@ -23,13 +25,15 @@ exports.search = (req, res) => {
               query: "2019"
             }
           }
+        },
+        {
+          match: {
+            language: {
+              query: "english"
+            }
+          }
         }
       ]
-    };
-    sort[0] = {
-      reviews_from_users: {
-        order: "desc"
-      }
     };
   }
 
@@ -42,12 +46,19 @@ exports.search = (req, res) => {
 
   //Search for Movies by Title, Date of published, Duration and Director
   if (title) {
+    let title_token = title.split(" ");
+    let minimum_should_match;
+    if (title_token.length >= 3) {
+      minimum_should_match = 3;
+    } else {
+      minimum_should_match = title_token.length;
+    }
     query.bool.must[must_index++] = {
       match: {
         title: {
-          query: req.query.title,
-          minimum_should_match: 3,
-          fuzziness: 2
+          query: title,
+          operator: "and",
+          fuzziness: "auto"
         }
       }
     };
@@ -57,7 +68,7 @@ exports.search = (req, res) => {
     query.bool.must[must_index++] = {
       match: {
         date_published: {
-          query: req.query.date_published
+          query: date_published
         }
       }
     };
@@ -67,7 +78,7 @@ exports.search = (req, res) => {
     query.bool.must[must_index++] = {
       match: {
         duration: {
-          query: req.query.duration
+          query: duration
         }
       }
     };
@@ -77,9 +88,9 @@ exports.search = (req, res) => {
     query.bool.must[must_index++] = {
       match: {
         director: {
-          query: req.query.director,
-          minimum_should_match: 3,
-          fuzziness: 2
+          query: director,
+          operator: "and",
+          fuzziness: "auto"
         }
       }
     };
@@ -90,7 +101,7 @@ exports.search = (req, res) => {
     query.bool.filter[filter_index++] = {
       match: {
         country: {
-          query: req.query.language
+          query: country
         }
       }
     };
@@ -100,7 +111,7 @@ exports.search = (req, res) => {
     query.bool.filter[filter_index++] = {
       match: {
         genre: {
-          query: req.query.genre
+          query: genre
         }
       }
     };
@@ -110,38 +121,8 @@ exports.search = (req, res) => {
     query.bool.filter[filter_index++] = {
       match: {
         language: {
-          query: req.query.language
+          query: language
         }
-      }
-    };
-  }
-
-  //Sort By Date
-  if (sort_date && sort_date_asc) {
-    sort[0] = {
-      date_published: {
-        order: "asc"
-      }
-    };
-  } else if (sort_date && !sort_duration) {
-    sort[0] = {
-      date_published: {
-        order: "desc"
-      }
-    };
-  }
-
-  //Sort by Duration
-  if (sort_duration && sort_duration_asc) {
-    sort[0] = {
-      duration: {
-        order: "asc"
-      }
-    };
-  } else if (sort_duration && !sort_duration_asc) {
-    sort[0] = {
-      duration: {
-        order: "desc"
       }
     };
   }
@@ -150,21 +131,94 @@ exports.search = (req, res) => {
     return esClient.search({ index: index, body: body });
   };
 
-  // only for testing purposes
-  // all calls must be initiated through the module
   const test = function test() {
+    let from_index = 0;
+    if (page) {
+      if (page > 0) {
+        from_index = page - 1;
+      }
+    }
     let body = {
-      size: 20,
-      from: (req.query.page - 1 || 0) * 20,
+      size: 1000,
+      from: from_index,
       query,
       sort
     };
+    console.log(body);
 
     search("library", body)
       .then(results => {
-        res.json(results.hits.hits);
+        let output = results.hits.hits;
+
+        //Sort by Reviews from users (for initial list of movies)
+        if (!title && !date_published && !genre && !duration && !country && !director && !language && !sort_duration && !sort_date) {
+          output.sort((a, b) => {
+            let movieA_reviews_from_users = 0;
+            if (a._source.reviews_from_users.length > 0) {
+              movieA_reviews_from_users = parseFloat(a._source.reviews_from_users.length);
+            }
+            let movieA_reviews_from_critics = 0;
+            if (a._source.reviews_from_critics.length > 0) {
+              movieA_reviews_from_critics = parseFloat(b._source.reviews_from_critics);
+            }
+            let movieA_worlwide_gross_income = 0;
+            if (a._source.worlwide_gross_income.length > 0) {
+              movieA_worlwide_gross_income = parseFloat(a._source.worlwide_gross_income.slice(2, a._source.worlwide_gross_income.length));
+            }
+
+            let movieB_reviews_from_users = 0;
+            if (b._source.reviews_from_users.length > 0) {
+              movieB_reviews_from_users = parseFloat(b._source.reviews_from_users.length);
+            }
+            let movieB_reviews_from_critics = 0;
+            if (b._source.reviews_from_critics.length > 0) {
+              movieB_reviews_from_critics = parseFloat(b._source.reviews_from_critics);
+            }
+            let movieB_worlwide_gross_income = 0;
+            if (b._source.worlwide_gross_income.length > 0) {
+              movieB_worlwide_gross_income = parseFloat(b._source.worlwide_gross_income.slice(2, b._source.worlwide_gross_income.length));
+            }
+
+            console.log(movieB_reviews_from_users + movieB_reviews_from_critics + movieB_worlwide_gross_income - (movieA_reviews_from_users + movieA_reviews_from_critics + movieA_worlwide_gross_income));
+            return movieB_reviews_from_users + movieB_reviews_from_critics + movieB_worlwide_gross_income - (movieA_reviews_from_users + movieA_reviews_from_critics + movieA_worlwide_gross_income);
+          });
+        }
+
+        //Sort by Date
+        if (sort_date === "true" && sort_date_asc === "true") {
+          output.sort((a, b) => {
+            let dateA = new Date(a._source.date_published),
+              dateB = new Date(b._source.date_published);
+            return dateA - dateB;
+          });
+        } else if (sort_date === "true" && sort_date_asc === "false") {
+          output.sort((a, b) => {
+            let dateA = new Date(a._source.date_published),
+              dateB = new Date(b._source.date_published);
+            return dateB - dateA;
+          });
+        }
+
+        //Sort by Duration
+        if (sort_duration === "true" && sort_duration_asc === "true") {
+          output.sort((a, b) => {
+            return a._source.duration - b._source.duration;
+          });
+        } else if (sort_duration === "true" && sort_duration_asc === "false") {
+          output.sort((a, b) => {
+            return b._source.duration - a._source.duration;
+          });
+        }
+
+        const page = parseInt(req.query.page) || 1;
+        const pager = paginate(output.length, page, 20);
+        const pageOfMovies = output.slice(pager.startIndex, pager.endIndex + 1);
+        res.json({ status: 200, pager, pageOfMovies });
       })
-      .catch(console.error);
+      .catch(err => {
+        console.log(err);
+        res.json({ status: 400, data: "Something went wrong" });
+      });
   };
 
   test();
